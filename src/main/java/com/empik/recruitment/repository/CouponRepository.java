9abc8cee60558e23fee1391f33c8a94b21c70082
karolinaszymanska <karolina.sz1993@gmail.com>
2026-03-08 16:05:16 +0100

    init commit

---
+++ src/main/java/com/empik/recruitment/repository/CouponRepository.java
@@ -0,0 +1,13 @@
+package com.empik.recruitment.repository;
+
+import com.empik.recruitment.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
